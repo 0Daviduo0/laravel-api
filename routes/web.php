114ea65ec52a8fec72d25dc392dfeecff853d142
Daviduo <davide0400@gmail.com>
2023-02-15 21:45:02 +0100

    added all routes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,16 @@ use App\Http\Controllers\MainController;
 Route::get('/', [MainController::class, 'home'])->name('home');
 Route::get('/movie', [MainController::class, 'movie'])->name('home.movie');
 
+//crea Film
+Route::get('/movie/create', [MainController::class, 'create'])->name('movie.create');
+//salva il Film
+Route::post('/movie/create', [MainController::class, 'store'])->name('movie.store');
+
+//modifica Film
+Route::get('/movie/edit/{movie}', [MainController::class, 'edit'])-> name('movie.edit');
+//aggiorna Film
+Route::post('/movie/edit/{movie}', [MainController::class, 'update'])-> name('movie.update');
+
+//elimina Film
+Route::get('/movie/delete/{movie}', [MainController::class, 'delete'])-> name('movie.delete');
+
