530b56644725d477cc8be7489824652a119d5491
Daviduo <davide0400@gmail.com>
2023-02-15 21:57:22 +0100

    added link to home and to film list

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
     <h1>Film</h1>
+    <h5>Per creare, modificare e eliminare film <a href="{{ route('home.movie') }}">Clicca qui</a></h5>
 
     @foreach ($genres as $genre)
         <h2> Genere: {{ $genre-> name }}</h2>
@@ -12,11 +13,12 @@
                 <li>
 
                     Film: {{ $movie -> name }} | Anno: {{ $movie -> year }} <br>
+                    Incassi: {{ $movie -> cashOut }}$ <br>
 
                     @foreach ($movie -> tags as $tag)
                         Tag: {{ $tag -> name }} | 
                     @endforeach
-                    
+
                 </li> <br> <br>
             @endforeach
         </ul>
--- resources/views/pages/subpages/movie.blade.php
+++ resources/views/pages/subpages/movie.blade.php
@@ -3,14 +3,18 @@
 @section('content')
 
     <h1>Lista Film</h1>
+    <h5>Per tornare alla Home dei film <a href="{{ route('home') }}">Clicca qui</a></h5>
+    <a href="{{ route('movie.create') }}">Aggiungi un nuovo film</a>
+
 
     @foreach ($movies as $movie)
 
-        <h3> Titolo: {{ $movie-> name }} | {{ $movie-> year }}</h3> 
+        <h3> Titolo: {{ $movie-> name }} | {{ $movie-> year }}</h3>
 
         <p>
 
-            {{ $movie -> genre -> name }}
+            Incassi: {{ $movie -> cashOut }}$ <br>
+            Genere: {{ $movie -> genre -> name }}
             <br>
             @foreach ($movie -> tags as $tag)
                 Tag: {{ $tag -> name }} | 
