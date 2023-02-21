bed8c6a4f8ad7688ce80a3cc295a21098a23d0bb
Daviduo <davide0400@gmail.com>
2023-02-21 10:58:49 +0100

    new version with only 1 form

--- frontend/vue-laravel-microservice/src/App.vue
+++ frontend/vue-laravel-microservice/src/App.vue
@@ -2,7 +2,15 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1/';
+//link const
+const API_URL = 'http://localhost:8000/api/';
+const API_VER = 'v1/';
+const API = API_URL + API_VER;
+
+//reset form
+const FORM_CLEANER = {
+  tags_id: []
+};
 
 export default {
 
@@ -10,346 +18,192 @@ export default {
     
     return {
 
-      force_update: false,
-
-      movies_bk: [],
       movies: [],
       genres: [],
       tags: [],
 
-      movie_name: '',
-      movie_year: '',
-      movie_cashOut: '',
+      newMovie: { ...FORM_CLEANER },
 
-      movie_genre: '',
-      movie_tags: [],
-
-      createMovie_variable: false,
-      updateMovie_variable: -1,
-      
-      test: true
+      //form activation
+      state: {
+        enableForm: false
+      }
     }
   },
+
   methods: {
 
-    // function that set's a boolean value to activate/disable movie creation
-    activateCreateMovie() {
+    //submit movie
+    Submit(e) {
 
-      this.createMovie_variable = true;
-    },
-    disableCreateMovie() {
-
-      this.createMovie_variable = false;
-    },
-
-    // function that set's a boolean value to activate/disable movie update
-    activateUpdateMovie(id) {
-      
-      const movie = this.getMovieById(id);
-      this.updateMovie_variable = id;
-
-      for (let x=0;x<this.tags.length;x++) {
-
-        const tag = this.tags[x];
-        if (this.tagFinder(movie, tag)) {
-
-          this.movie_tags.push(tag.id);
-        }
-      }
-    },
-    disableUpdateMovie() {
-
-      this.updateMovie_variable = -1;
-    },
-
-    // function to ceate movies
-    createMovie(e) {
-
+      //prevent page refresh
       e.preventDefault();
 
-      const movie = this.buildMovie();
+      const newMovie = this.newMovie;
+      const doesExist = API + ('id' in newMovie ? 'movie/update/' + this.newMovie.id : 'movie/store'); //if is an existent movie pass movie id to link
 
-      axios.post(API_URL + 'movie/store', movie)
-        .then(res => {
+      // console.log(newMovie);
+      // console.log(doesExist);
 
-          const data = res.data;
-          const success = data.success;
-
-          if (success) {
-
-            this.clearForms();
-            this.updateMovies();
-          }
-        }).catch(err => console.error(err));
-    },
-
-    // function to stop the creating movie process
-    cancelCreateMovie(e) {
-
-      e.preventDefault();
-      this.clearForms();
-    },
-
-    // function to update a movie
-    updateMovie(e) {
-
-      e.preventDefault();
-
-      const movie = this.getMovieById(this.updateMovie_variable);
-      movie['tags_id'] = this.movie_tags;
-      
-      axios.post(API_URL + 'movie/update/' + this.updateMovie_variable, movie)
-          .then(res => {
+      //axios call
+      axios.post(doesExist, newMovie).then(res => {
 
             const data = res.data;
             const success = data.success;
 
             if (success) {
 
-              this.disableUpdateMovie();
-              this.updateMovies();
+              this.closeForm();
+              this.Update();
+
             }
-          }).catch(err => console.error(err));
-    },
-
-    // function to stop the updating movie process
-    cancelUpdateMovie(e) {
-
-      e.preventDefault();
-      this.disableUpdateMovie();
-    },
-
-    // function to delete a movie
-    deleteMovie(id) {
-
-      axios.get(API_URL + 'movie/delete/' + id)
-           .then(res => {
-
-            const data = res.data;
-            const success = data.success;
-            
-            if (success) {
-
-              this.updateMovies();
-            }
-           })
-           .catch(err => console.error(err));
-    },
-
-    // function to clear the forms (if not the forms remains with older text)
-    clearForms() {
-
-      this.movie_name = '';
-      this.movie_year = '';
-      this.movie_cashOut = '';
-      this.movie_genre = '';
-      this.movie_tags = [];
-
-      this.disableCreateMovie();
+        }).catch(err => console.log);
     },
     
-    // retrieve new movies list
-    updateMovies() {
+    //function to edit an existing movie
+    Edit(movie) {
 
-      axios.get(API_URL + 'movie/all')
-         .then(res => {
+      //copy movie information to form
+      this.newMovie = { ...movie };
 
-            console.log(res);
+      //get tags
+      this.newMovie.tags_id = [];
+      for (let i=0;i<movie.tags.length;i++) { //cicle tags
+        const tag = movie.tags[i];
+        this.newMovie.tags_id.push(tag.id);
+      }
 
-            const data = res.data;
-            const success = data.success;
-            const response = data.response;
+      //activate form
+      this.state.enableForm = true;
 
-            const movies = this.movies_bk = response.movies;
-            const genres = response.genres;
-            const tags = response.tags;
-
-            if (success) {
-
-              this.movies = movies;
-              this.genres = genres;
-              this.tags = tags;
-            }
-         })
-         .catch(err => console.error(err));
     },
 
-    // function to retrieve tags associated to a movie
-    tagFinder(movie, tag) {
+    //function to delete a movie
+    Delete(movie) {
 
-      for (let x=0;x<movie.tags.length;x++) {
+      //axios call
+      axios.delete(API + 'movie/delete/' + movie.id).then(res => { 
 
-        const movieTag = movie.tags[x];
-        
-        if (movieTag.id == tag.id) {
+        const data = res.data; //movie data
+        const success = data.success; //operation state
 
-          return true;
+        if (success) this.Update();
+
+      }).catch(err => console.log); //get errors
+    },
+
+    closeForm() {
+
+      this.newMovie = { ...FORM_CLEANER };
+      this.state.enableForm = false;
+
+    },
+
+    //update movies data
+    Update() {
+
+      axios.get(API + 'movie/all').then(res => {
+
+        const data = res.data;
+        const success = data.success;
+        const response = data.response;
+
+        const movies = response.movies;
+        const genres = response.genres;
+        const tags = response.tags;
+
+        if (success) {
+
+          this.movies = movies;
+          this.genres = genres;
+          this.tags = tags;
+
         }
-      }
-
-      return false;
-    },
-
-    // function that puts togheter all movie informations
-    buildMovie() {
-
-      return {
-
-        'name': this.movie_name,
-        'year': this.movie_year,
-        'cashOut': this.movie_cashOut,
-        'genre_id': this.movie_genre,
-        'tags_id': this.movie_tags,
-      };
-    },
-
-    // function that retrieve a movie by his id
-    movieIdFinder(id) {
-
-      for (let x=0;x<this.movies.length;x++) {
-
-        const movie = this.movies[x];
-
-        if (movie.id == id)
-          return x;
-      }
-
-      return null;
-    },
-    getMovieById(id) {
-
-      return this.movies[this.movieIdFinder(id)];
+      }).catch(err => console.log);
     }
   },
+
+  //action when page loads
   mounted() {
 
-    this.updateMovies();
+    this.Update();
+
   }
+
 };
 
 </script>
 
 <template>
   <div class="allWrapper">
+
     <h1>LISTA FILM</h1>
+    
+    <div class="formContainer" v-if="state.enableForm"> <!-- se il form è true -->
+    
+      <form> 
+        <label for="name">Titolo</label>
+          <input type="text" name="name" v-model="newMovie.name"> <br>
 
-    <!-- Form to create movies -->
-    <div class="addNewMovies_Container">
+        <label for="year">Anno</label>
+          <input type="number" name="year" v-model="newMovie.year"> <br>
 
-      <form v-if="createMovie_variable">
-        <label for="name"> <i class="fa-solid fa-ticket"></i> | TITOLO</label>
-          <input type="text" name="name" v-model="movie_name"> <br>
-        <label for="year"> <i class="fa-regular fa-calendar-check"></i> | ANNO</label>
-          <input type="number" name="year" v-model="movie_year"> <br>
-        <label for="cashOut"> <i class="fa-solid fa-money-bill-trend-up"></i> | INCASSI</label>
-          <input type="number" name="cashOut" v-model="movie_cashOut">
+        <label for="cashOut">Incassi</label>
+          <input type="number" name="cashOut" v-model="newMovie.cashOut"> <br>
 
-        <div class="genreContainer propertiesContainers">
-
-          <label class="basicLabel" for="genre_id"> <i class="fa-solid fa-list-ul"></i> | GENERE </label>
-
-          <select name="genre_id" v-model="movie_genre">
-            <!-- retrieve genre name -->
-            <option v-for="genre in genres" :key="genre.id" :value="genre.id">
-              {{ genre.name }}
-            </option>
-          </select> 
-
-        </div>
-
-        <div class="tagsContainer">
-
-          <label class="basicLabel"> <i class="fa-solid fa-tags"></i> | ETICHETTE </label> <br>
-          
-          <!-- Tag container -->
+        <label for="genre">Genere</label>
+          <select name="genre_id" v-model="newMovie.genre_id">
+            <option v-for="genre in genres" :key="genre.id" :value="genre.id">{{ genre.name }}</option>
+          </select> <br>
+        
+        <label>Etichette:</label>
           <div v-for="tag in tags" :key="tag.id">
-            <!-- tags already associated are checked -->
-            <input type="checkbox" @click="updateCheckbox" :value="tag.id" :id="tag.id" v-model="movie_tags">
-              <label :for="tag.id"> {{ tag.name }} </label> <br>
+            <input type="checkbox" :id="'tag-' + tag.id" :value="tag.id" v-model="newMovie.tags_id">
+            <label :for="'tag-' + tag.id">{{ tag.name }}</label>
           </div>
 
-        </div>
-
-        <!-- button to cancel the movie creation -->
-        <button @click="cancelCreateMovie">ANNULLA</button>
-
-        <!-- button to create a movie based on the form -->
-        <input @click="createMovie" type="submit" value="CREATE NEW MOVIE">
-
+        <!-- cancel button -->
+        <button @click="closeForm">ANNULLA</button>
+        <!-- submit/update button -->
+        <input type="submit" @click="Submit" :value="'id' in newMovie ? 'AGGIORNA FILM: ' + newMovie.id : 'CREA NUOVO FILM'"> <!-- if id exist then modify button to edit -->
       </form>
 
-      <button v-else @click="activateCreateMovie">CREA UN NUOVO FILM</button>
-
     </div>
-    
-    <!-- LIST START -->
-    <div class="moviesWrapper">
-      <!-- retrieve movie list -->
-      <div class="movieContainer" v-for="movie in movies" :key="movie.id">
 
-        <div class="movieTitle"> <h2>{{ movie.name }}</h2> </div>
 
-        <!-- options to interact -->
-        <div class="actionButtons" v-if="updateMovie_variable != movie.id">
-          <!-- modify movie -->
-          <button @click="activateUpdateMovie(movie.id)"> <i class="fa-regular fa-pen-to-square"></i> </button>
-          <!-- delete movie -->
-          <button @click="deleteMovie(movie.id)"> <i class="fa-regular fa-trash-can"></i> </button> 
-        </div>
-        <!-- form to modify movie -->
-        <form v-else>
-            <label for="name"> <i class="fa-solid fa-ticket"></i> | TITOLO</label>
-            <input type="text" name="name" v-model="movie.name">
-            <br>
-            <label for="year"> <i class="fa-regular fa-calendar-check"></i> | ANNO</label>
-            <input type="number" name="year" v-model="movie.year">
-            <br>
-            <label for="cashOut"> <i class="fa-solid fa-money-bill-trend-up"></i> | INCASSI</label>
-            <input type="number" name="cashOut" v-model="movie.cashOut">
-            <br>
+    <div v-else> <!-- se il form è false -->
 
-            <!-- GENRES -->
-            <div class="genreContainer propertiesContainers">
+      <button @click="state.enableForm = true">CREA UN NUOVO FILM</button>
 
-              <label class="basicLabel" for="genre_id"> <i class="fa-solid fa-list-ul"></i> | GENERE </label>
+      <div class="moviesWrapper">
 
-              <select name="genre_id" v-model="movie.genre_id">
-                <option v-for="genre in genres" :key="genre.id" :value="genre.id"> {{ genre.name }} </option>
-              </select>
-            </div>
-
-            <!-- TAGS -->
-            <div class="tagsContainer">
-
-              <label class="basicLabel"> <i class="fa-solid fa-tags"></i> | ETICHETTE </label>
-
-              <div class="tagsContainer" v-for="tag in tags" :key="tag.id">
-                <input class="rightSpacing" @click="updateCheckbox" type="checkbox" :value="tag.id" :id="tag.id" :checked="tagFinder(movie, tag)" v-model="movie_tags">
-                  <label :for="tag.id"> {{ tag.name }} </label> <br>
-              </div>
-
-            </div>
+        <div class="movieContainer" v-for="movie in movies" :key="movie.id">
             
+            {{ movie.name }} <br>
 
-            <!-- UNDO - SAVE -->
-            <div class="actionButtons">
+            <button @click="Edit(movie)">MODIFICA</button>
+            <button @click="Delete(movie)">CANCELLA</button>
 
-              <button @click="cancelUpdateMovie"> Annulla </button>
-              <input @click="updateMovie" type="submit" :value="'Aggiorna film: ' + movie.id">
-
-            </div>
-        </form>
+            <ul>
+              <li v-for="tag in movie.tags" :key="tag.id"> {{ tag.name }} </li>
+            </ul>
+            
+        </div>
+        
       </div>
 
     </div>
+
   </div>
 </template>
 
 <style scoped>
 
-  .allWrapper{
+.allWrapper{
     width: 1280px;
-  }
+    display: flex;
+    flex-direction: column;
+}
 
   .moviesWrapper{
     display: grid;
@@ -367,32 +221,5 @@ export default {
     background-color: rgba(255, 255, 255, 0.185);
   }
 
-  .movieTitle{
-    width: 200px;
-    text-align: center;
-  }
-
-  .basicLabel{
-    font-weight: 700;
-    font-size: 18px;
-  }
-
-  .actionButtons{
-    display: flex;
-    width: 200px;
-    justify-content: center;
-  }
-
-  .propertiesContainers{
-    margin-top: 20px;
-  }
-
-  .tagsContainer{
-    width: 200px;
-  }
-
-  .rightSpacing{
-    margin-right: 10px;
-  }
 
 </style>
